0df87e52e86597b84f8b53520161e4d59d6a8e5a
Trevor Slocum <tslocum@gmail.com>
2014-09-05 22:58:39 -0700

    Only build source package

--- package_linux.sh
+++ package_linux.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-python setup.py --command-packages=stdeb.command bdist_deb
+python setup.py --command-packages=stdeb.command sdist_dsc
 # TODO: automate debuild -S -sa in source dir
 # TODO: automate dput ppa:tslocum/aafm <*_source>.changes
